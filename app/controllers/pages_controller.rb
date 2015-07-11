52d0f8a0c3b961117e7e3823c8bee53ae365e1dd
Marie-Lou Blanchard <mloublanchard@gmail.com>
2015-07-11 13:42:36 -0400

    install bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def product
+  end
+
+  def purpose
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "pages#home"
+ get "purpose" => "pages#purpose"
+ get "contact" => "pages#contact"
+ get "product" => "pages#product"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
