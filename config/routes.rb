fadb6f7a04da11e99a9cdb182393f47bfe9ded39
Marie-Lou Blanchard <mloublanchard@gmail.com>
2015-07-11 14:02:27 -0400

    make principals pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
  get "purpose" => "pages#purpose"
  get "contact" => "pages#contact"
  get "product" => "pages#product"
+ get "blog" => "pages#blog"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
