083119504210f509875e2fbc3865e11eee0a7fb0
Marie-Lou Blanchard <mloublanchard@gmail.com>
2015-07-16 16:02:35 -0400

    add devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Note to set this to your actual host
+  config.action_mailer.default_url_options = { host: 'mariesmind.com' }
 end
